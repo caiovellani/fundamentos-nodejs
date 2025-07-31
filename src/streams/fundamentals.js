1d23ea3bb91d763a0ec664c3963c238e5055fcc2
Caio Vellani <caiovellani@outlook.com>
2025-07-30 22:02:28 -0300

    WIP: node streams Writable

--- src/streams/fundamentals.js
+++ src/streams/fundamentals.js
@@ -1,6 +1,6 @@
 // Streams
 
-import { Readable } from 'node:stream'
+import { Readable, Writable } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -20,4 +20,11 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream().pipe(new MultiplyByTenStream())
